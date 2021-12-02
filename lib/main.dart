8b54c9f335de110e335bcdea96145a0de4f1a8ff
J.A. Loquacious <jlkds@pm.me>
2021-12-02 14:58:59 -0500

    Move landing page to separate intro screen file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:globo_fitness/screens/intro_screen.dart';
 
 void main() {
   runApp(const GlobeApp());
@@ -9,40 +10,8 @@ class GlobeApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Globo Fitness')),
-        body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/atlas.jpg'),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: Center(
-              child: Container(
-            padding: EdgeInsets.all(24),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-              color: Colors.white70,
-            ),
-            child: Text(
-              'Commit to be fit, dare to be great \nwith Globo Fitness!',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 22,
-                shadows: [
-                  Shadow(
-                    blurRadius: 2.0,
-                    color: Colors.grey,
-                    offset: Offset(1.0, 1.0),
-                  ),
-                ],
-              ),
-            ),
-          )),
-        ),
-      ),
+    return const MaterialApp(
+      home: IntroScreen()
     );
   }
 }
