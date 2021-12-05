abd0f61a9a5dca0588c20866d6d26997e000ac66
J.A. Loquacious <jlkds@pm.me>
2021-12-05 06:57:41 -0500

    Draw list tile items in menu drawer

--- lib/shared/menu_drawer.dart
+++ lib/shared/menu_drawer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../screens/intro_screen.dart';
+import '../screens/bmi_screen.dart';
 
 class MenuDrawer extends StatelessWidget {
   const MenuDrawer({Key? key}) : super(key: key);
@@ -31,12 +33,25 @@ class MenuDrawer extends StatelessWidget {
       ),
     );
 
-    menuTitles.forEach((String element) {
+    for (var element in menuTitles) {
+      Widget screen = Container();
       menuItems.add(ListTile(
         title: Text(element, style: const TextStyle(fontSize: 18)),
-        onTap: () {},
+        onTap: () {
+          switch (element) {
+            case 'Home':
+              screen = const IntroScreen();
+              break;
+            case 'BMI Calculator':
+              screen = const BmiScreen();
+              break;
+          }
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => screen)
+          );
+        },
       ));
-    });
+    }
     return menuItems;
   }
 }
