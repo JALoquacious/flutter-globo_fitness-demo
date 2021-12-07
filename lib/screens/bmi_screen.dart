ef623bfd6b12a41c3f875aaab5ab4251fa89e3f3
J.A. Loquacious <jlkds@pm.me>
2021-12-07 08:39:50 -0500

    Finish BMI screen UI by adding height & weight text fields and result button

--- lib/screens/bmi_screen.dart
+++ lib/screens/bmi_screen.dart
@@ -10,9 +10,13 @@ class BmiScreen extends StatefulWidget {
 }
 
 class _BmiScreenState extends State<BmiScreen> {
+  TextEditingController txtHeight = TextEditingController();
+  TextEditingController txtWeight = TextEditingController();
   final double fontSize = 18;
   late List<bool> isSelected;
   String result = '';
+  String heightMessage = '';
+  String weightMessage = '';
   bool isMetric = true;
   bool isImperial = false;
   double? height;
@@ -26,6 +30,9 @@ class _BmiScreenState extends State<BmiScreen> {
 
   @override
   Widget build(BuildContext context) {
+    heightMessage = 'Please enter your height in ' + (isMetric ? 'meters' : 'inches');
+    weightMessage = 'Please enter your weight in ' + (isMetric ? 'kilos' : 'pounds');
+
     return Scaffold(
       appBar: AppBar(title: const Text('BMI Calculator')),
       drawer: const MenuDrawer(),
@@ -44,21 +51,38 @@ class _BmiScreenState extends State<BmiScreen> {
               ),
             ],
             isSelected: [isMetric, isImperial],
-            onPressed: (value) {
-              if (value == 0) {
-                isMetric = true;
-                isImperial = false;
-              } else {
-                isMetric = false;
-                isImperial = true;
-              }
-              setState(() {
-                isSelected = [isMetric, isImperial];
-              });
-            },
+            onPressed: toggleMeasure,
           ),
+          TextField(
+            controller: txtHeight,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(hintText: heightMessage)
+          ),
+          TextField(
+            controller: txtWeight,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(hintText: weightMessage)
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Calculate BMI', style: TextStyle(fontSize: fontSize))
+          ),
+          Text(result, style: TextStyle(fontSize: fontSize))
         ],
       ),
     );
+  }
+
+  void toggleMeasure(value) {
+    if (value == 0) {
+      isMetric = true;
+      isImperial = false;
+    } else {
+      isMetric = false;
+      isImperial = true;
+    }
+    setState(() {
+      isSelected = isSelected;
+    });
   }
 }
