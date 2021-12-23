d91bf831ad69b27a2534e2a90ff02d212c67b2d3
J.A. Loquacious <jlkds@pm.me>
2021-12-23 06:40:55 -0500

    Deserialize retrieved JSON into Weather object

--- lib/data/http_helper.dart
+++ lib/data/http_helper.dart
@@ -1,14 +1,19 @@
+import 'package:globo_fitness/data/weather.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HttpHelper {
   final String authority = 'api.openweathermap.org';
   final String path = 'data/2.5/weather';
   final String apiKey = '1fba682936bb6f2d7e388f0756e23df2';
 
-  Future<String> getWeather(String location) async {
+  Future<Weather> getWeather(String location) async {
     Map<String, dynamic> parameters = {'q': location, 'appId': apiKey};
     Uri uri = Uri.https(authority, path, parameters);
     http.Response result = await http.get(uri);
-    return result.body;
+    Map<String, dynamic> data = json.decode(result.body);
+    Weather weather = Weather.fromJson(data);
+
+    return weather;
   }
 }
